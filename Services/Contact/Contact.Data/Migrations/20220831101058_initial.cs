1f0aa5e4b852b62d4e6de0fd15003332170f0d31
Muhammet Ali Tütüncü <malitutuncu@gmail.com>
2022-08-31 13:11:14 +0300

    Migration eklendi

---
+++ Services/Contact/Contact.Data/Migrations/20220831101058_initial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contact.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    company_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_informations",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    information_type = table.Column<int>(type: "integer", maxLength: 50, nullable: false),
+                    information_content = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_informations", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_informations_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_informations_user_id",
+                table: "user_informations",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_informations");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
